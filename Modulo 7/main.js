1f72d07802ff806eb668c6f04dd190ba0f016e22
leonardocestari <leoclacerda@gmail.com>
2022-12-29 17:03:08 -0300

    exercicio modulo 07

---
+++ Modulo 7/main.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('formulario')
+const inputUm = document.getElementById('numeroUm')
+const inputDois = document.getElementById('numeroDois')
+
+const numeroUmErro = document.getElementById('numeroUmValidator')
+const numeroDoisErro = document.getElementById('numeroDoisValidator')
+
+const mensagemSucesso = document.getElementById('formSucesso')
+const mensagemErro = document.getElementById('formErro')
+
+function validarForm(a, b) {
+    return parseInt(b) > parseInt(a);
+}
+
+function validarInput(target, textErro) {
+    const validar = target.value == ''
+    if(validar){
+        textErro.style.display = 'block'
+    } else {
+        textErro.style.display = 'none'
+    }
+    return !validar
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const validarUm = validarInput(inputUm, numeroUmErro)
+    const validarDois = validarInput(inputDois, numeroDoisErro)
+    if(validarUm && validarDois && validarForm(inputUm.value, inputDois.value)){
+        mensagemSucesso.style.display = 'block'
+        mensagemErro.style.display = 'none'
+        inputUm.value = ''
+        inputDois.value = ''
+    }
+    else {
+        mensagemSucesso.style.display = 'none'
+        mensagemErro.style.display = 'block'
+    }
+})
